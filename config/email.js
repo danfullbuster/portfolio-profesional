e294fc1812996e4f07609615ae12a42c75594e63
Angie Daniela Bohorquez Escovar <angiedanielabe@hotmail.com>
2025-11-22 01:24:18 -0500

    Mejorar configuracion de Gmail

--- config/email.js
+++ config/email.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 
 // Configurar transporter de Nodemailer con Gmail
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // true para 465, false para otros puertos
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
